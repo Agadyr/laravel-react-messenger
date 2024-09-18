cfa3555f56c95677f77614be3082e576ae288a5a
Agadyr <kairambekovmadi@gmail.com>
2024-09-18 15:26:15 +0500

    Two functions have been implemented in the MessageController, and a MessageResource has been created.

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -3,21 +3,40 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreMessageRequest;
+use App\Http\Resources\MessageResource;
 use App\Models\Group;
 use App\Models\Message;
 use App\Models\User;
-use Illuminate\Http\Request;
+use Laravel\Reverb\Pulse\Livewire\Messages;
 
 class MessageController extends Controller
 {
     public function byUser(User $user)
     {
+        $messages = Message::where('sender_id', auth()->id())
+            ->where('receiver_id', $user->id)
+            ->orWhere('sender_id', $user->id)
+            ->where('receiver_id', auth()->id())
+            ->latest()
+            ->paginate(10);
+
+        return inertia('Home', [
+            'selectedConversation' => $user->toConversationArray(),
+            'messages' => MessageResource::collection($messages)
+        ]);
 
     }
 
     public function byGroup(Group $group)
     {
+        $messages = Messages::where('group_id', $group->id)
+            ->Latest()
+            ->paginate(50);
 
+        return inertia('Home', [
+            'selectedConversation' => $group->toConversationArray(),
+            'messages' => MessageResource::collection($messages)
+        ]);
     }
 
     public function loadOrder(Message $message)
